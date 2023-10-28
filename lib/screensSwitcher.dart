7734780b97da8d1bca11dadadf6eca6e4cbc1932
CapBox <abdullahbanafea3@gmail.com>
2023-10-28 00:07:47 +0000

    made navigationBar to switch between screens fixed some errors in register screen and login screen

---
+++ lib/screensSwitcher.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:lafyuu/screens/homePage.dart';
+
+class ScreensSwitcher extends StatefulWidget {
+  const ScreensSwitcher({super.key});
+
+  @override
+  State<ScreensSwitcher> createState() => _ScreensSwitcherState();
+}
+
+class _ScreensSwitcherState extends State<ScreensSwitcher> {
+  int selectedPage = 0;
+  List<Widget> screens = [
+    HomePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: screens[selectedPage]
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedPage,
+        onTap: (page) {
+          setState(() {
+            selectedPage = page;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search_outlined), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: 'Cart'),
+          BottomNavigationBarItem(icon: Icon(Icons.local_offer_outlined), label: 'Offers'),
+          BottomNavigationBarItem(icon: Icon(Icons.person_2_outlined), label: 'Account'),
+        ],
+      ),
+    );
+  }
+}
